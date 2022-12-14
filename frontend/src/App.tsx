0fc73b75cd10e664671c05ebe76ef58a6f88ce94
unknown <sirinthip99978@gmail.com>
2022-12-14 14:15:20 +0700

    controller - close #5

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          SE <code>src/App.tsx</code> and save to reload.
+          SEE <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
